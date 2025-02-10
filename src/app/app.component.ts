addaeea5a9efbc7f25b635c9cbad960ac3077344
David MTZD <davidmtzd21512@gmail.com>
2025-02-09 20:20:54 -0600

    se importo el componente padre al app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteInlineComponent } from "./componente-inline/componente-inline.component";
 import { ComponenteInterpolacionComponent } from "./componente-interpolacion/componente-interpolacion.component";
+import { PadreComponent } from "./padre/padre.component";
+import { HijoComponent } from './padre/hijo/hijo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, ComponenteInterpolacionComponent],
+  imports: [ ComponenteInlineComponent, ComponenteInterpolacionComponent, PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
