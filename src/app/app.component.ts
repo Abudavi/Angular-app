ec1b090d3aeb26318d61b16eb71c9a1c6ef7357d
David MTZD <davidmtzd21512@gmail.com>
2025-02-09 19:40:03 -0600

    se agregó el componente de interpolación al módulo principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NuevoComponenteComponent } from "./nuevo-componente/nuevo-componente.component";
 import { ComponenteInlineComponent } from "./componente-inline/componente-inline.component";
+import { ComponenteInterpolacionComponent } from "./componente-interpolacion/componente-interpolacion.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent],
+  imports: [RouterOutlet, NuevoComponenteComponent, ComponenteInlineComponent, ComponenteInterpolacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
