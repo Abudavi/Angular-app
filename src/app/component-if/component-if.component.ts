7a39b42801ff310cb41c491f4f630448d3e8c7a0
David MTZD <davidmtzd21512@gmail.com>
2025-02-12 16:06:43 -0600

    se creó el componente ComponentIf con su lógica y plantilla

---
+++ src/app/component-if/component-if.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component-if',
+  imports: [],
+  templateUrl: './component-if.component.html',
+  styleUrl: './component-if.component.css'
+})
+export class ComponentIfComponent {
+
+}
