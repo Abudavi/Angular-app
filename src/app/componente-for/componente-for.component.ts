b636ca5b35df50bd38d301354eb3107cb841a218
David MTZD <davidmtzd21512@gmail.com>
2025-02-12 17:20:06 -0600

    se agregó lógica para agregar tareas y se definió un título en el componente ComponenteFor

--- src/app/componente-for/componente-for.component.ts
+++ src/app/componente-for/componente-for.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './componente-for.component.css'
 })
 export class ComponenteForComponent {
+titulo='Directiva for en Angular';
+tareas: string[] = ['Tarea 1', 'Tarea 2dd', 'Tarea 3', 'Tarea 4', 'Tarea 5'];
 
+agregarTarea(tarea: string): void{
+  if(tarea){
+    this.tareas.push(tarea);
+  }
+}
 }
