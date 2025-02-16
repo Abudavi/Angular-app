9ef412ffaed6358ebd5eda1185dbefeb213443dc
David MTZD <davidmtzd21512@gmail.com>
2025-02-16 17:04:02 -0600

    se agregó el componente ViewChild con su plantilla y estilos

---
+++ src/app/view-child/view-child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-child',
+  imports: [],
+  templateUrl: './view-child.component.html',
+  styleUrl: './view-child.component.css'
+})
+export class ViewChildComponent {
+
+}
